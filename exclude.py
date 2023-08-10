15a2663913b9652f27c903723b2ba46b0f612cca
MyNameIsCarsten <108253422+MyNameIsCarsten@users.noreply.github.com>
2023-08-10 08:41:02 +0200

    Update exclude.py

--- exclude.py
+++ exclude.py
@@ -1,7 +1,47 @@
-# This list is not complete and can be appended to your liking. Currently, it stores german words.
+# This list is not complete and can be appended to your liking.
 
-exclude = ["die", "der", "ist", "eine", "in", "und", "zu", "ein", "für", "von", "kann",
-           "einer", "wird", "das", "es", "den", "man", "als", "des", "einem", "um", "eines", "werden", "dass", "sich",
-           "bei", "durch", "im", "zum", "oder", "und", "an","so", "diese", "auf", "mittels", "zur", "was", "unserer", "handelt",
-           "zuvor", "allerdings", "kann", "dann", "bei", "hierbei", "daher", "mit", "sind", "wie", "auch", "am", "nicht", "kann",
-           "dieser", "dies", "hat", "aus", "nach", "sein", "sie", "können", "dabei", "dieses", "meist"]
+exclude = [
+    # Articles
+    "a", "an", "the",
+
+    # Pronouns
+    "I", "you", "he", "she", "it", "we", "they", "me", "him", "her", "us", "them",
+    "my", "your", "his", "its", "our", "their", "mine", "yours", "hers", "ours", "theirs",
+    "myself", "yourself", "himself", "herself", "itself", "ourselves", "yourselves", "themselves",
+
+    # Prepositions
+    "about", "above", "across", "after", "against", "along", "amid", "among", "around",
+    "as", "at", "before", "behind", "below", "beneath", "beside", "between", "beyond",
+    "by", "concerning", "considering", "despite", "down", "during", "except", "for",
+    "from", "in", "inside", "into", "like", "near", "of", "off", "on", "onto", "out",
+    "outside", "over", "past", "regarding", "round", "since", "through", "throughout",
+    "till", "to", "toward", "under", "underneath", "until", "unto", "up", "upon", "with",
+    "within", "without",
+
+    # Conjunctions
+    "and", "or", "but", "so", "for", "nor", "yet", "after", "although", "as", "because",
+    "before", "if", "once", "since", "than", "that", "though", "till", "unless", "until",
+    "when", "whenever", "where", "whereas", "wherever", "while",
+
+    # Other Fill Words
+    "all", "any", "both", "each", "few", "many", "much", "more", "most", "other", "same",
+    "several", "such", "what", "which", "who", "whom", "whose", "which", "where", "when",
+    "why", "how", "until", "before", "after", "because", "while", "though", "although",
+    "unless", "since", "until", "when", "where", "whether", "if", "since", "as", "so",
+    "than", "that", "even", "just", "still", "almost", "already", "nearly", "now", "then",
+    "there", "here", "also", "either", "neither", "both", "not only", "whether", "while",
+    "until", "rather", "quite", "such", "too", "much", "less", "fewer", "more", "only",
+    "enough", "too", "very", "this",
+
+    # Verbs
+    "is", "are", "were", "be", "was",
+
+    # Adjectives
+    "well",
+
+    # Numbers
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+
+    # Others
+    "not",
+]
